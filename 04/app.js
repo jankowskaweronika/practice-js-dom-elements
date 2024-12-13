0e2c0ed4598fc79a3281062f8c6a71eb69d78a10
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-13 22:49:12 +0100

    task 4

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,21 @@
 console.log('DOM');
 
-// struktura do wykorzystania w pętli
+const nav = document.querySelector('nav');
+const ulElement = document.createElement('ul');
+
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    { text: 'start', url: '/' },
+    { text: 'galeria', url: '/gallery' },
+    { text: 'kontakt', url: '/contact' },
 ];
+menuItems.forEach((item) => {
+    const liElement = document.createElement('li');
+    const aElement = document.createElement('a');
+
+    liElement.appendChild(aElement);
+    ulElement.appendChild(liElement);
+    aElement.innerText = item.text;
+    aElement.href = item.url;
+});
+
+nav.appendChild(ulElement);
